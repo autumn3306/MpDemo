f9bcb7600bba1b148d00768ea1e24b8f74b15615
luqiuyun <autumn_svip@163.com>
2022-07-01 16:41:57 +0800

    third commit

--- src/main/java/com/itheima/entity/User.java
+++ src/main/java/com/itheima/entity/User.java
@@ -1,18 +1,29 @@
 package com.itheima.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
     private String name;
     private Integer age;
     private String email;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;  //create_time
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime; //update_time
+
 }
--- src/test/java/com/itheima/MpDemoApplicationTests.java
+++ src/test/java/com/itheima/MpDemoApplicationTests.java
@@ -13,17 +13,41 @@ class MpDemoApplicationTests {
     @Autowired
     private UserMapper userMapper;
 
+    /**
+     * 查询所有
+     */
     @Test
-    public  void TestFindAll(){
+    public void TestFindAll() {
         List<User> users = userMapper.selectList(null);
         System.out.println(users);
     }
 
+    /**
+     * 添加一条记录
+     */
     @Test
-    public  void TestAdd(){
-      User user = new User(null,"小李",25,"1234@163.com");
+    public void TestAdd() {
+        User user = new User();
+        user.setAge(25);
+        user.setEmail("1234@163.com");
+        user.setName("小明");
+
         int insert = userMapper.insert(user);
-        System.out.println(insert);
+        System.out.println(insert);  //受影响行数
+    }
+
+    /**
+     * 更新一条记录
+     */
+    @Test
+    public void TestUpdate() {
+        User user = new User();
+        user.setId(1542785490560405505L);
+        user.setAge(25);
+        user.setEmail("1234@163.com");
+        user.setName("小明");
+        int update = userMapper.updateById(user);
+        System.out.println(update);
     }
 
 }
